75771006904f422ef7666e24d95f3b5bafa922ee
VG <vg@Villages-MacBook-Pro.local>
2020-05-21 21:06:58 +0200

    Level 6 - OAuth

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -16,31 +18,61 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+//Passport session
 app.use(session({
   secret: "A smalo plesent of some lingelie",
   resave: false,
   saveUninitialized: false
 }));
 
+//Passport app.use
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Mongoose DB connect
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
-
+//Passport plugin- mongoose Schema
 userSchema.plugin(passportLocalMongoose);
+//findOrCreate plugin
+userSchema.plugin(findOrCreate);
 
+//Mongoose model
 const User = new mongoose.model("User", userSchema);
 
+//Passport
 passport.use(User.createStrategy());
+//Passport serialize and deserialize
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+//Google OAuth
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -70,6 +102,17 @@ app.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
 })
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
 
 app.post("/register", function(req, res) {
 
